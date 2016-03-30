59330f32d3932325ce0769e07e88ca89ee164f31
Jonathan Huang <jonwhuang@gmail.com>
2016-03-30 12:08:57 -0700

    Change navbar - tablesorter doesn't work after link to home

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,8 @@
 $(document).ready(function() {
   // call the tablesorter plugin
   $("[data-sort=table]").tablesorter({
-    // Sort on the second column, in ascending order
     sortList: [[3,0]]
   });
 });
+
+
