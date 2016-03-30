a53c9db51cf49c28ac018834abdbf4b92b49434d
Jonathan Huang <jonwhuang@gmail.com>
2016-03-30 13:47:31 -0700

    Create new return working

--- app/controllers/returns_controller.rb
+++ app/controllers/returns_controller.rb
@@ -11,6 +11,22 @@ class ReturnsController < ApplicationController
   end
 
   def create
+    @return = Return.new(return_params)
 
+    if @return.save
+      flash[:notice] = "Return successfully created"
+      redirect_to home_path
+    else
+      render 'new'
+    end
+  end
+
+
+
+
+  private
+
+  def return_params
+    params.require(:return).permit(:auth_number, :return_date, :paid_date, :credit_memo_number, :brand_id, :manufacturer_id, :frame_count, :comments)
   end
 end
