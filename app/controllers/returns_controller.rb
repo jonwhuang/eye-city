15caa3e080ea82079046b232a97285aae6c16429
Jonathan Huang <jonwhuang@gmail.com>
2016-03-30 14:14:11 -0700

    Add update and delete functions for returns

--- app/controllers/returns_controller.rb
+++ app/controllers/returns_controller.rb
@@ -7,8 +7,16 @@ class ReturnsController < ApplicationController
     @returns = Return.all
   end
 
+  def show
+    @return = Return.find(params[:id])
+  end
+
   def new
     @return = Return.new
+  end
+
+  def edit
+    @return = Return.find(params[:id])
   end
 
   def create
@@ -20,6 +28,24 @@ class ReturnsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @return = Return.find(params[:id])
+
+    if @return.update(return_params)
+      flash[:notice] = "Return successfully updated"
+      redirect_to home_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @return = Return.find(params[:id])
+    @return.destroy
+    flash[:notice] = "Return successfully deleted"
+    redirect_to home_path
   end
 
   private
