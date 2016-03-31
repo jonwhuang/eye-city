fe15901d4fb9f57bd2429c8e9b2461456a45f969
Jonathan Huang <jonwhuang@gmail.com>
2016-03-31 15:41:55 -0700

    Update migrations for validations

--- db/migrate/20160329220329_create_returns.rb
+++ db/migrate/20160329220329_create_returns.rb
@@ -1,13 +1,13 @@
 class CreateReturns < ActiveRecord::Migration
   def change
     create_table :returns do |t|
-      t.string :auth_number
-      t.date :return_date
+      t.string :auth_number, null: false
+      t.date :return_date, null: false
       t.date :credit_date
       t.string :credit_memo_number
-      t.integer :brand_id
-      t.integer :manufacturer_id
-      t.integer :frame_count
+      t.integer :brand_id, null: false
+      t.integer :manufacturer_id, null: false
+      t.integer :frame_count, null: false
       t.string :comments
 
       t.timestamps null: false
