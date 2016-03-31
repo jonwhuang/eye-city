70cbcc5f19e3f0baee5318882ae19e63fb41787d
Jonathan Huang <jonwhuang@gmail.com>
2016-03-30 18:19:37 -0700

    Show page for returns

--- app/models/return.rb
+++ app/models/return.rb
@@ -3,6 +3,10 @@ class Return < ActiveRecord::Base
   belongs_to :manufacturer
 
   def credited?
-    self.credit_memo_number
+    self.credit_memo_number != ""
+  end
+
+  def days_outstanding
+    (Date.today - self.return_date).to_i
   end
 end
